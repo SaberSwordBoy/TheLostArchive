aea18af14583ed2171daab79b7ad710e28ec2ce3
SaberSwordBoy <sabercodeboy@gmail.com>
2022-04-29 23:58:17 +0000

    Add logs/logging clean.py

---
+++ logs/clean.py
@@ -0,0 +1,17 @@
+import sys
+
+ips = ["208.115.199.26", "54.36.108.162", "51.222.107.173"]
+
+try:
+	lines = []
+	file = open(sys.argv[1], "r")
+	for line in file.readlines():
+		for ip in ips:
+			if not ip in line: lines.append(line)
+			elif ip in line:
+				continue
+	with open(sys.argv[2], "w") as f:
+		f.writelines(lines)
+except:
+	print("Usage: python3 clean.py <file>")
+
