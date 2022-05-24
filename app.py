70fcfda8a79ac0cf11eeaee02d6145133e429c1c
sabercodegirl <saberfilmsproductions@gmail.com>
2022-05-24 10:56:40 -0400

    ACTUALLY Fix upload page

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def upload():
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             
-            return redirect(url_for('btsdownloads'))
+            return redirect(url_for('behind_the_scenes'))
     return '''
     <!doctype html>
     <title>Upload new File</title>
