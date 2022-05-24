a2ad3892bf652b68268b1ef4d439702242b6f2c2
sabercodegirl <saberfilmsproductions@gmail.com>
2022-05-24 10:54:36 -0400

    Fix upload page

--- app.py
+++ app.py
@@ -273,7 +273,8 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
-            return redirect(url_for('download_file', name=filename))
+            
+            return redirect(url_for('bts'))
     return '''
     <!doctype html>
     <title>Upload new File</title>
